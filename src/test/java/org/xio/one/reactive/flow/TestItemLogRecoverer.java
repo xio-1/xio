fd15d9d82060e4518bd8ae3238e5e450198b63a5
xio <xio@xio.one>
2024-11-05 20:41:09 +0800

    wip

--- src/test/java/org/xio/one/reactive/flow/TestItemLogRecoverer.java
+++ src/test/java/org/xio/one/reactive/flow/TestItemLogRecoverer.java
@@ -6,6 +6,12 @@ import org.xio.one.reactive.flow.domain.item.logging.ItemLogFlowRecovery;
 
 public class TestItemLogRecoverer implements ItemLogFlowRecovery<String, String> {
 
+  public TestItemLogRecoverer(String filename) {
+
+
+  }
+
+
   @Override
   public void recoverAllItem(Flow<String, String> flow, ItemDeserializer<String> deserializer) {
 
